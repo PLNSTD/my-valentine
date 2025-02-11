86970cc2ca64ccfebee3650bcc029602fd95b847
Plidher <65611683+PLNSTD@users.noreply.github.com>
2025-02-10 23:40:49 -0500

    Added Images

--- src/components/WhyILoveYou.jsx
+++ src/components/WhyILoveYou.jsx
@@ -1,9 +1,35 @@
 import { useState } from "react";
 import cupidIcon from "../assets/images/cupid.png";
+import exhibitImg from "../assets/images/exhibit.jpg";
+import accomplishmentsImg from "../assets/images/accomplishments.jpg";
+import peaceImg from "../assets/images/peace.jpg";
+import distanceImg from "../assets/images/distance.jpg";
 import LoveCard from "./LoveCard";
 import Polaroid from "./Polaroid";
 
-const remembrances = ["Fig1", "Fig2", "Fig3", "Fig4"];
+// const remembrances = ["Fig1", "Fig2", "Fig3", "Fig4"];
+const remembrances = [
+  {
+    imgUrl: exhibitImg,
+    imgDescription:
+      "Among all the wonders in that museum, you were the most captivating exhibit of all.",
+  },
+  {
+    imgUrl: accomplishmentsImg,
+    imgDescription:
+      "You illuminate my accomplishements, no matter how small, and fill me with pride in every little victory.",
+  },
+  {
+    imgUrl: peaceImg,
+    imgDescription:
+      "You bring peace into my world, ignite hope within me, and turn every gray into golden sunlight",
+  },
+  {
+    imgUrl: distanceImg,
+    imgDescription:
+      "You shaped my mind, empowering me to overcome any distance. With each passing day, I fall deeper in Love",
+  },
+];
 
 const WhyILoveYou = () => {
   const [isCardOpen, setIsCardOpen] = useState(false);
